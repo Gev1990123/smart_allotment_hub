0e2f6552288c867fb6fe595d425a707a86df9ac4
Matt Gevaux <matt.gevaux@hotmail.com>
2026-02-04 09:34:14 +0000

    updated logic.py to enhance logging format

--- logic/logic.py
+++ logic/logic.py
@@ -4,6 +4,7 @@ import requests
 import logging
 import json
 import paho.mqtt.client as mqtt
+import sys
 
 
 # -------------------------
@@ -19,12 +20,18 @@ MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
 
 DEVICE_ID = "SA-NODE1"
 
+
 logger = logging.getLogger("logic")
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
 )
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 # -------------------------
 # MQTT publish helper
